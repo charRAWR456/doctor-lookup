d1ffb7d1bbc489e93e3d2d6317576871b6647aca
Jamie Pittak <charrawr456@gmail.com>
2018-03-23 09:01:04 -0700

    add form for user to input criteria in index.html and add jQuery to retrieve that information in main.js

--- src/main.js
+++ src/main.js
@@ -3,3 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import './../.env';
 import $ from 'jquery';
+
+$(document).ready(function() {
+
+  $("#doctor-requested-info").submit(function(event){
+    event.preventDefault();
+
+    let location = $("#address").val();
+    let medicalSymptom = $("#medical-symptom").val();
+    let doctorName = $("#doctor-name").val();
+
+    $('#address').val("");
+    $("#medical-symptom").val("");
+    $("#doctor-name").val("");
+  });
+});
