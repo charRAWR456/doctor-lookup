b30859c6946fd637a2fcb6a5279b8ea0b87bfb3d
Jamie Pittak <charrawr456@gmail.com>
2018-03-23 16:12:43 -0700

    start to incorporate API into project

--- src/main.js
+++ src/main.js
@@ -16,5 +16,19 @@ $(document).ready(function() {
     $('#address').val("");
     $("#medical-symptom").val("");
     $("#doctor-name").val("");
+
+
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&specialty_uid=${medicalSymptom}&location=${location}&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=4eb8980f81fbd52ac494fd65e2bd5a1b`).then(function(response){
+      console.log(response);
+      $('#doctor-results').append("doctor name: "+ response.data.profile.first_name + response.data.profile.last_name)
+    })
+    .fail(function(error) {
+          $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+
+})
+
+
+
+
   });
 });
