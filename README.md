ed9f405631d181779d187676993cc6e84a30cf69
Guest User <Guest@epicodus-4.local>
2018-03-27 07:53:35 -0700

    add functionality and address details for location on result page

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This webpage is designed for users to enter a medical issue they are experiencin
 1. Clone this repository
 2. go to https://developer.betterdoctor.com/ and login or create an account to access you API Key
 3. Once you have obtained you API key, open the project in an editor of your choosing
-4. Create a .env file in the root of the project with API_KEY=[Your API key goes here] then save
+4. Create a .env file in the root of the project with exports.apiKey=["Your API key goes here"] then save
 5. You can now close your editor and type npm install in your terminal
 6. After npm install is finished, type npm run start in your terminal and the application will launch
 7. If your API request is not successful and receive an "Origin is not allowed by Access-Control-Allow-Origin" error in your console, try installing the Allow-Control-Allow-Origin: * Chrome extension.
--- src/main.js
+++ src/main.js
@@ -22,8 +22,12 @@ $(document).ready(function() {
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&specialty_uid=${medicalSymptom}&location=${location}&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=4eb8980f81fbd52ac494fd65e2bd5a1b`).then(function(response){
       console.log(response);
       for(let i = 0; i < response.data.length; i++){
+        for(let j = 0; j < response.data[i].practices.length; j++){
 
-        $('#doctor-results').append(`<img src=${response.data[i].profile.image_url}>` +"<br><strong>Doctor Name: </strong>"+ response.data[i].profile.first_name + " " + response.data[i].profile.last_name + ", "+ response.data[i].profile.title + '<br><strong> Bio: </strong>' + response.data[i].profile.bio + '<hr>')
+            $('#doctor-results').append(`<img src=${response.data[i].profile.image_url}>` +"<br><strong>Doctor Name: </strong>"+ response.data[i].profile.first_name + " " + response.data[i].profile.last_name + ", "+ response.data[i].profile.title + '<br><strong> Location: </strong>'+ response.data[i].practices[0].visit_address.street +", "+ response.data[i].practices[0].visit_address.city +", "+ response.data[i].practices[0].visit_address.state +'<br><strong> Bio: </strong>' + response.data[i].profile.bio + '<hr>')
+            $('#doctor-results').show();
+            $('#doctor-requested-info').hide();
+        }
       }
       })
       .fail(function(error) {
